ecec352b685436b2e6181ed9d21fe5249990fc43
Jacob <levinson1214@gmail.com>
2018-04-21 15:31:47 -0400

    Add files via upload
    initial commit setting up file reader

---
+++ Grephy.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+public class Grephy{
+  public static void main(String[] args) throws IOException {
+    Grephy grephy = new Grephy();
+    for (String fileName : args) {
+      File file = new File(fileName);
+      grephy.handleFile(file);
+    }
+  }
+  
+  public void handleFile(File file) throws IOException {
+    try (FileReader reader = new FileReader(file);
+         // buffer for efficiency
+         BufferedReader buffer = new BufferedReader(reader)) {
+           handleCharacters(buffer);
+         }
+  }
+  
+  public void handleCharacters(BufferedReader reader) throws IOException {
+    
+    StringBuffer strBuffer = new StringBuffer();
+    String line;
+    //takes the contents of file line by line
+    while ((line = reader.readLine()) != null) {
+      strBuffer.append(line);
+      strBuffer.append("\n");
+    }
+    String contents = strBuffer.toString();
+    //loops through the file in the form of a string
+    for(int i = 0; i < contents.length(); i++) {
+    }
+  }
+}
