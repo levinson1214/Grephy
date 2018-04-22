409ffec360ade793049e5fd9f9b35eda10abb2f6
Jacob <levinson1214@gmail.com>
2018-04-22 02:13:38 -0400

    Add files via upload
    got creative with some dialogue, program can read file, identify regex pattern in the file, and output those lines

--- Grephy.java
+++ Grephy.java
@@ -7,6 +7,14 @@ public class Grephy{
     Grephy grephy = new Grephy();
     regex = args[0];
     File file = new File(args[1]);
+    System.out.println("Rick: Welcome to Grephy!\n");
+    System.out.println("Morty: J-j-jeez Grandpa Rick, w-what's Grephy?\n");
+    System.out.println("Rick: Well Morty, *belch* I wouldn't expect you to comprehend the advanced capabilities of Grephy.");
+    System.out.println("But if you must ask, it is a program that reads and learns the alphabet of a file, converts a Regex to");
+    System.out.println("an NFA, converts that into a DFA, then uses the *belch* DFA to test for matches in the file line by line.\n");
+    System.out.println("Morty: That just sounds like grep with extra steps.\n");
+    System.out.println("Rick: Shut up Morty, this is beyond your intellectual capabilities.\nSpaceship, what's the Regex we are looking for?\n");
+    System.out.println("Spaceship: The Regex that was inputted was: '" + regex + "'\n");
     grephy.handleFile(file);
   }
   
@@ -24,6 +32,10 @@ public class Grephy{
     String line;
     //takes the contents of file line by line
     while ((line = reader.readLine()) != null) {
+      int test = line.indexOf(regex);
+      if(test >= 0){
+        System.out.println(line);
+      }
       strBuffer.append(line);
       strBuffer.append("\n");
     }
