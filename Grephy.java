1433a5171b3d884055e17498118054201db3bba0
Jacob <levinson1214@gmail.com>
2018-04-26 00:47:30 -0400

    Add files via upload
    working on reading alphabet from file

--- Grephy.java
+++ Grephy.java
@@ -2,12 +2,17 @@ import java.util.*;
 import java.io.*;
 public class Grephy{
   public static String regex;
+  public static int regLength;
+  public static int matches;
+  public ArrayList<Character> alphabet = new ArrayList<Character>();
   
   public static void main(String[] args) throws IOException {
     Grephy grephy = new Grephy();
     regex = args[0];
     File file = new File(args[1]);
-    System.out.println("Rick: Welcome to Grephy!\n");
+    regLength = regex.length();
+    System.out.println("Rick: Spaceship, activate Grephy!\n");
+    System.out.println("Spaceship: Welcome to Grephy!\n");
     System.out.println("Morty: J-j-jeez Grandpa Rick, w-what's Grephy?\n");
     System.out.println("Rick: Well Morty, *belch* I wouldn't expect you to comprehend the advanced capabilities of Grephy.");
     System.out.println("But if you must ask, it is a program that reads and learns the alphabet of a file, converts a Regex to");
@@ -32,16 +37,51 @@ public class Grephy{
     String line;
     //takes the contents of file line by line
     while ((line = reader.readLine()) != null) {
-      int test = line.indexOf(regex);
-      if(test >= 0){
-        System.out.println(line);
-      }
+      /*int test = line.indexOf(regex);
+       if(test >= 0){
+       System.out.println(line);
+       matches ++;
+       }*/
       strBuffer.append(line);
       strBuffer.append("\n");
     }
+    //System.out.println("\nSpaceship: File Search Complete!");
+    /*if(matches == 1){
+     System.out.println("1 match found.");
+     }else{
+     System.out.println(matches + " matches found.");
+     }*/
     String contents = strBuffer.toString();
+    alphabet = new ArrayList<Character>();
+    //this will be used to keep track of alphabet
+    Map<Character, Integer> treeMap = new TreeMap<Character, Integer>();
+    for (char i = 'a'; i <= 'z'; i++) {
+      treeMap.put(i, 0);
+    }
+    
+    char[] contentsChar = contents.toCharArray();
+    
+    for (int i = 0; i < contentsChar.length; i++) {
+      // make it lower case
+      char ch = Character.toLowerCase(contentsChar[i]);
+      // just get the value and update it by one
+      // check for characters only
+      if (treeMap.containsKey(ch)) {
+        treeMap.put(ch, treeMap.get(ch) + 1);
+      }
+    }
+    
+// print the count
+    for (char key : treeMap.keySet()) {
+      int count = treeMap.get(key);
+      if (count > 0) {
+        alphabet.add(key);
+        System.out.println(key + ":" + treeMap.get(key));
+      }
+    }
     //loops through the file in the form of a string
     for(int i = 0; i < contents.length(); i++) {
+      
     }
   }
 }
